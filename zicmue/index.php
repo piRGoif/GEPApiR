92af6438ac7a61e639e3c16ada7bc0ba175c5114
Pierre Goiffon <pierre.goiffon@pirhome.net>
2024-08-20 21:05:31 +0200

    Musiques photos : correction du lien La Valdingue

--- zicmue/index.php
+++ zicmue/index.php
@@ -274,7 +274,7 @@ Actuellement (rentrée 2024), j'ai l'immense plaisir de jouer avec :</p>
 			<li>Albums des Fanfaronnades (festival de fanfares grenoblois organisé par Pink It Black) : <a href="https://www.flickr.com/photos/pirgoif/sets/72177720307334752/">2023</a>, <a href="https://www.flickr.com/photos/pirgoif/sets/72177720297685805/">2022</a>, <a href="https://www.flickr.com/photos/pirgoif/sets/72157706831924795/">2019</a>, <a href="https://www.flickr.com/photos/pirgoif/sets/72157713972430191/">2018</a></li>
 			<li>2023 : <a href="https://www.flickr.com/photos/pirgoif/albums/72177720312177018">Les 10 ans des Son'enBulles</a></li>
 			<li>2022 : <a href="https://www.flickr.com/photos/pirgoif/albums/72177720300537588">Anniversaire des Garces Embouchées</a></li>
-			<li>2021 : <a href="https://www.flickr.com/photos/pirgoif/albums/72157719802471828">La Valdingue 2021</a></li>
+			<li>2021 : <a href="https://www.flickr.com/photos/pirgoif/albums/72157719802578248">La Valdingue 2021</a></li>
 			<li>2014 : <a href="https://www.flickr.com/photos/pirgoif/albums/72157646265826332">Fort en son 6</a></li>
 		</ul>
 	</li>
