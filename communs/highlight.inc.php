6fb93e46e916333f3c691e4135cf46380c9bef59
Pierre Goiffon <pierre.goiffon@pirhome.net>
2025-02-16 16:22:19 +0100

    Highlight.js : chg thème de monokai à monokai-sublime

--- communs/highlight.inc.php
+++ communs/highlight.inc.php
@@ -3,7 +3,7 @@
  Intégration du code hightliter Highlight.js (https://highlightjs.org/)
 *******************************************************************************/
 ?>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/monokai.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/monokai-sublime.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
 
 <style>
