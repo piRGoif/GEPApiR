30c1a2ba152ac3842ab1aa3ad756746f8f5dc792
Pierre Goiffon <pierre.goiffon@pirhome.net>
2026-08-11 21:50:19 +0200

    CSS callouts notes en violet

--- communs/changelog.inc.php
+++ communs/changelog.inc.php
@@ -9,6 +9,9 @@ class GepapirChangelog {
     public static function getChangelogEntries()
     {
         return [
+            new ChangelogEntry('2026-08-11', '9.1.5', [
+                'CSS : callouts note en violet',
+            ]),
             new ChangelogEntry('2026-08-06', '9.1.4', [
                 'Informatique Linux Commandes : réorganisation de la section commandes usuelles',
                 'Informatique Linux Commandes : ajout redirections',
